805a8a3461f68d16e5270cab82ceb219cc4d2b13
Poornimaduttargi <duttargipoornima.com>
2026-03-25 18:17:14 +0530

    Step 14: Converted tags into vectors using CountVectorizer

--- movie_recommendation.py
+++ movie_recommendation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import ast
+from sklearn.feature_extraction.text import CountVectorizer
 
 movies = pd.read_csv('tmdb_5000_movies.csv')
 credits = pd.read_csv('tmdb_5000_credits.csv')
@@ -15,8 +16,45 @@ def convert(text):
     for i in ast.literal_eval(text):
         L.append(i['name'])
     return L
+def convert_cast(text):
+    L = []
+    counter = 0
+    for i in ast.literal_eval(text):
+        if counter < 3:
+            L.append(i['name'])
+            counter += 1
+        else:
+            break
+    return L
+def fetch_director(text):
+    L = []
+    for i in ast.literal_eval(text):
+        if i['job'] == 'Director':
+            L.append(i['name'])
+    return L
+def remove_space(L):
+    return [i.replace(" ", "") for i in L]
 
 movies['genres'] = movies['genres'].apply(convert)
 movies['keywords'] = movies['keywords'].apply(convert)
+movies['cast'] = movies['cast'].apply(convert_cast)
+movies['crew'] = movies['crew'].apply(fetch_director)
+movies['overview'] = movies['overview'].apply(lambda x: x.split())
+movies['genres'] = movies['genres'].apply(remove_space)
+movies['keywords'] = movies['keywords'].apply(remove_space)
+movies['cast'] = movies['cast'].apply(remove_space)
+movies['crew'] = movies['crew'].apply(remove_space)
+movies['tags'] = movies['overview'] + movies['genres'] + movies['keywords'] + movies['cast'] + movies['crew']
+new_df = movies[['movie_id','title','tags']]
 
-print(movies.head())
+new_df['tags'] = new_df['tags'].apply(lambda x: " ".join(x))
+cv = CountVectorizer(max_features=5000, stop_words='english')
+vectors = cv.fit_transform(new_df['tags']).toarray()
+
+
+# print(movies[['title','cast']].head())
+# print(movies[['title','crew']].head())
+# print(movies[['title','overview']].head())
+print(movies[['title','tags']].head())
+print(new_df.head())
+print(vectors.shape)
