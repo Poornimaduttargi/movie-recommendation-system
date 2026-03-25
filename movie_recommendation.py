8f0ce7008ea2ce4c7ed1c21f25105a59f9066135
Poornimaduttargi <duttargipoornima.com>
2026-03-25 17:37:10 +0530

    Step 7: Converted genres and keywords from JSON to list

---
+++ movie_recommendation.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import ast
+
+movies = pd.read_csv('tmdb_5000_movies.csv')
+credits = pd.read_csv('tmdb_5000_credits.csv')
+
+movies = movies.merge(credits, on='title')
+
+movies = movies[['movie_id','title','overview','genres','keywords','cast','crew']]
+
+movies.dropna(inplace=True)
+
+def convert(text):
+    L = []
+    for i in ast.literal_eval(text):
+        L.append(i['name'])
+    return L
+
+movies['genres'] = movies['genres'].apply(convert)
+movies['keywords'] = movies['keywords'].apply(convert)
+
+print(movies.head())
